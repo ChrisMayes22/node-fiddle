5b546f229ba671ba4059ee71cc131216107553b7
Chris Mayes <coachchristac@gmail.com>
2019-02-12 12:42:42 -0500

    Added Update Method, Optimized code for CRUD app
    IN INDEX.JS
    --Changed if-chain to a switch statement for readability
    IN NOTES.JS
    --Added a fetchNotes helper function for reusability & clarity
    --Simplified add method to contain only one if/else statement
    --Added an update method using logic similar to add method

--- index.js
+++ index.js
@@ -8,16 +8,22 @@ const notes = require('./notes');
 const argv = yargs.argv;
 const command = process.argv[2];
 
-if(command === 'add') {
-    notes.add(argv.title, argv.body);
-} else if (command === 'list') {
-    notes.list();
-} else if (command === 'read') {
-    notes.read(argv.title);
-} else if (command === 'delete') {
-    notes.delete(argv.title);
-} else {
-    console.log('Command not recognized.')
-}
-
-// fs.appendFileSync('notes.js', 'function add(a, b){ return a + b }');
+switch(command){
+    case 'add':
+        notes.add(argv.title, argv.body);
+        break;
+    case 'update':
+        notes.update(argv.title, argv.body);
+        break;
+    case 'list':
+        notes.list();
+        break;
+    case 'read':
+        notes.read(argv.title);
+        break;
+    case 'delete':
+        notes.delete(argv.title);
+        break;
+    default:
+    console.log('Command not recognized')
+};
--- notes.js
+++ notes.js
@@ -1,31 +1,30 @@
 console.log('Notes Running')
 const fs = require('fs');
 
+function fetchNotes(){
+    try{
+        const notes = JSON.parse(fs.readFileSync('./playground/notes.json'));
+        return notes;
+    } catch (e){
+        const notes = [];
+        return notes;
+    }
+}
+
 module.exports = {
     add: (title, body) => {
-        const note = {title, body};
-        let notes = []
-        try{
-            notes = JSON.parse(fs.readFileSync('./playground/notes.json'));
-        } catch (e){
-
-        }
-        if(!notes[0]){
+        const note = {title, body}
+        const notes = fetchNotes();
+        if(!notes.find(item => item.title === note.title)){
             notes.push(note);
             fs.writeFileSync('./playground/notes.json', JSON.stringify(notes));
         } else {
-            notes.forEach(item => {
-            if(item.title !== title){
-                notes.push(note);
-                fs.writeFileSync('./playground/notes.json', JSON.stringify(notes));
-            } else {
-                console.log('ERR: An item with that title already exists.')
-            }
-        })}
+            console.log('ERR: An item with that title already exists.')
+        }
     },
     list: () => {
         console.log(`Listing all notes`);
-        const data = JSON.parse(fs.readFileSync('./playground/notes.json'));
+        const data = fetchNotes();
         data.forEach((item, i) => {
             console.log(`TITLE OF ITEM ${i +1}: ${item.title}`);
             console.log(`BODY OF ITEM ${i+1}: ${item.body}`);
@@ -33,7 +32,7 @@ module.exports = {
     },
     read: (title) => {
         console.log(`Reading note with title ${title}`);
-        const data = JSON.parse(fs.readFileSync('./playground/notes.json'));
+        const data = fetchNotes();
         const targetItem = data.find(item => item.title === title);
         if(targetItem){
             console.log(`BODY OF ITEM: ${targetItem.body}`)
@@ -43,7 +42,7 @@ module.exports = {
     }, 
     delete: (title) => {
         console.log(`Removing note with title ${title}`);
-        const data = JSON.parse(fs.readFileSync('./playground/notes.json'));
+        const data = fetchNotes();
         const targetIndex = data.findIndex(item => item.title === title);
         if(targetIndex !== -1){
             console.log('DATA BEFORE SPLICE:', data)
@@ -52,6 +51,17 @@ module.exports = {
             fs.writeFileSync('./playground/notes.json', JSON.stringify(data));
         } else {
             console.log('ERROR: Title not found');
+        }
+    },
+    update: (title, payload) => {
+        console.log(`Updating body of note with title ${title}`);
+        const notes = fetchNotes();
+        const target = notes.find(item => item.title === title);
+        if(target){
+            target.body = payload;
+            fs.writeFileSync('./playground/notes.json', JSON.stringify(notes));
+        } else {
+            console.log('ERR: An item with that title does not exist.')
         }
     }
 }
